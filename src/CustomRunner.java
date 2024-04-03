9bbe713f7c1062de9bb6fe2b8bf8a9dcf395a9c2
NehaSharma33 <neha_sharma3@epam.com>
2024-04-03 10:24:42 +0530

    testngTaskRelatedFiles

---
+++ src/CustomRunner.java
@@ -0,0 +1,16 @@
+import org.testng.TestNG;
+
+public class CustomRunner extends TestNG {
+
+    public CustomRunner(){
+        super();
+        this.addListener(new CustomListener());
+    }
+
+    public static void main(String[] args)
+    {
+        CustomRunner customRunner =new CustomRunner();
+        customRunner.setTestClasses(new Class[]{MyFunctionalityTest.class});
+        customRunner.run();
+    }
+}
